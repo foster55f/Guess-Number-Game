5abe1fc6b97ec39cd6ee5711d9fea702274e70a8
“Foster <fosterataylor@yahoo.com>
2020-04-16 11:02:56 -0600

    Initial Commit

---
+++ Guess Number Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess Number Game
+//
+//  Created by Foster Taylor on 4/16/20.
+//  Copyright © 2020 Foster Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
